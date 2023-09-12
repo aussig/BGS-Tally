e377040a19e67b6598187d8568a6fc3e09c36e78
Demon5760 <demon5760.alt@outlook.com>
2023-09-12 07:19:36 -0400

    variable name change per PR feedback

--- bgstally/state.py
+++ bgstally/state.py
@@ -34,7 +34,7 @@ class State:
         self.DiscordPostStyle:tk.StringVar = tk.StringVar(value=config.get_str('XDiscordPostStyle', default=DiscordPostStyle.EMBED))
         self.DiscordActivity:tk.StringVar = tk.StringVar(value=config.get_str('XDiscordActivity', default=DiscordActivity.BOTH))
         self.EnableOverlay:tk.StringVar = tk.StringVar(value=config.get_str('XEnableOverlay', default=CheckStates.STATE_ON))
-        self.EnableOverlayCurrentTick:tk.StringVar = tk.StringVar(value=config.get_str('XEnableOverlayCurrentTick', default=CheckStates.STATE_ON))
+        self.EnableOverlayCurrentTick:tk.StringVar = tk.StringVar(value=config.get_str('BGST_EnableOverlayCurrentTick', default=CheckStates.STATE_ON))
         self.EnableSystemActivityByDefault:tk.StringVar = tk.StringVar(value=config.get_str('BGST_EnableSystemActivityByDefault', default=CheckStates.STATE_ON))
 
         # Persistent values
@@ -76,7 +76,7 @@ class State:
         config.set('XDiscordPostStyle', self.DiscordPostStyle.get())
         config.set('XDiscordActivity', self.DiscordActivity.get())
         config.set('XEnableOverlay', self.EnableOverlay.get())
-        config.set('XEnableOverlayCurrentTick', self.EnableOverlayCurrentTick.get())
+        config.set('BGST_EnableOverlayCurrentTick', self.EnableOverlayCurrentTick.get())
         config.set('BGST_EnableSystemActivityByDefault', self.EnableSystemActivityByDefault.get())
 
         # Persistent values

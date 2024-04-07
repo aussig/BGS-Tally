10039a4b22c905bfdac77d13ebcc9700f7e83a2e
aussig <austin@opensceneryx.com>
2024-04-07 08:14:54 +0100

    Update version number for release

--- load.py
+++ load.py
@@ -8,7 +8,7 @@ from bgstally.constants import UpdateUIPolicy
 from bgstally.debug import Debug
 
 PLUGIN_NAME = "BGS-Tally"
-PLUGIN_VERSION = semantic_version.Version.coerce("3.6.0")
+PLUGIN_VERSION = semantic_version.Version.coerce("3.6.1")
 
 # Initialise the main plugin class
 this:BGSTally = BGSTally(PLUGIN_NAME, PLUGIN_VERSION)

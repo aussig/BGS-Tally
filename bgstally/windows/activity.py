3887d1adee2bdabfc96193e448690fcd6695628c
aussig <austin@opensceneryx.com>
2024-05-08 18:31:01 +0100

    Change text on activity post type dropdown to allow distinguishing in Italian. Update all translation strings.

--- bgstally/windows/activity.py
+++ bgstally/windows/activity.py
@@ -68,7 +68,7 @@ class WindowActivity:
         self.btn_post_to_discord: ttk.Button = ttk.Button(frm_buttons, text=_("Post to Discord"), command=partial(self._post_to_discord, activity), # LANG: Button label
                                                           state=(tk.NORMAL if self._discord_button_available() else tk.DISABLED))
         self.btn_post_to_discord.pack(side=tk.RIGHT, padx=5, pady=5)
-        activity_type_options: dict = {DiscordActivity.BOTH: _("All"), # LANG: Dropdown menu on activity window
+        activity_type_options: dict = {DiscordActivity.BOTH: _("Both BGS and TW"), # LANG: Dropdown menu on activity window
                                        DiscordActivity.BGS: _("BGS Only"), # LANG: Dropdown menu on activity window
                                        DiscordActivity.THARGOIDWAR: _("TW Only")} # LANG: Dropdown menu on activity window
         activity_type_var: tk.StringVar = tk.StringVar(value=activity_type_options.get(self.bgstally.state.DiscordActivity.get(), DiscordActivity.BOTH))

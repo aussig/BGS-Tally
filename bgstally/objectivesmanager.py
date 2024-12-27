2eeb62f745a891be5e6ca76849f79b000bb28094
aussig <austin@opensceneryx.com>
2024-12-27 18:44:03 +0000

    Improve formatting for mission progress

--- bgstally/objectivesmanager.py
+++ bgstally/objectivesmanager.py
@@ -84,93 +84,114 @@ class ObjectivesManager:
                     case MissionType.EQUALISE: result += "  º " + "Equalise two Factions" + "\n"
 
                 for target in mission.get('targets', []):
-                    target_system = target.get('system', mission_system)
-                    target_station = target.get('station', mission_station)
-                    target_faction = target.get('faction', mission_faction)
-
-                    try:
-                        value: int = int(target.get('value', 1))
-                    except ValueError:
-                        value: int = 1
-                    try:
-                        progress: int = int(target.get('progress', 0))
-                    except ValueError:
-                        progress: int = 0
-
-                    if value > 0 and progress >= value:
-                        flag: str = "√ [done]"
-                        complete: bool = True
-                    else:
-                        flag: str = "•"
-                        complete: bool = False
+                    target_system: str = target.get('system', mission_system)
+                    target_station: str = target.get('station', mission_station)
+                    target_faction: str = target.get('faction', mission_faction)
+                    status: str
+                    value: int
 
                     match target.get('type'):
                         case MissionTargetType.VISIT:
+                            status, value = self._get_status(target, numeric=False)
                             if target_station:
-                                result += f"    {flag} Access the market in station '{target_station}' in '{target_system}'" + "\n"
+                                result += f"    {status} Access the market in station '{target_station}' in '{target_system}'" + "\n"
                             else:
-                                result += f"    {flag} Visit system '{target_system}'" + "\n"
+                                result += f"    {status} Visit system '{target_system}'" + "\n"
 
                         case MissionTargetType.INF:
-                            status: str = flag if complete else f"{flag} [{progress} / {value} INF]"
+                            status, value = self._get_status(target, label="INF")
                             if value > 0:
                                 result += f"    {status} Boost '{target_faction}' in '{target_system}'" + "\n"
                             elif value < 0:
                                 result += f"    {status} Undermine '{target_faction}' in '{target_system}'" + "\n"
                             else:
-                                result += f"    {flag} Boost '{target_faction}' in '{target_system}' with as much INF as possible" + "\n"
+                                result += f"    {status} Boost '{target_faction}' in '{target_system}' with as much INF as possible" + "\n"
 
                         case MissionTargetType.BV:
-                            status: str = flag if complete else f"{flag} [{human_format(progress)} / {human_format(value)} CR]"
+                            status, value = self._get_status(target, label="CR")
                             result += f"    {status} Hand in Bounty Vouchers for '{target_faction}' in '{target_system}'" + "\n"
 
                         case MissionTargetType.CB:
-                            status: str = flag if complete else f"{flag} [{human_format(progress)} / {human_format(value)} CR]"
+                            status, value = self._get_status(target, label="CR")
                             result += f"    {status} Hand in Combat Bonds for '{target_faction}' in '{target_system}'" + "\n"
 
                         case MissionTargetType.EXPL:
-                            status: str = flag if complete else f"{flag} [{human_format(progress)} / {human_format(value)} CR]"
+                            status, value = self._get_status(target, label="CR")
                             result += f"    {status} Hand in Exploration Data for '{target_faction}' in '{target_system}'" + "\n"
 
                         case MissionTargetType.TRADE_PROFIT:
-                            status: str = flag if complete else f"{flag} [{human_format(progress)} / {human_format(value)} CR]"
+                            status, value = self._get_status(target, label="CR")
                             result += f"    {status} Generate Trade Profit for '{target_faction}' in '{target_system}'" + "\n"
 
                         case MissionTargetType.BM_PROF:
-                            status: str = flag if complete else f"{flag} [{human_format(progress)} / {human_format(value)} CR]"
+                            status, value = self._get_status(target, label="CR")
                             result += f"    {status} Generate Black Market Profit for '{target_faction}' in '{target_system}'" + "\n"
 
                         case MissionTargetType.GROUND_CZ:
-                            status: str = flag if complete else f"{flag} [{progress} / {value} wins]"
+                            status, value = self._get_status(target, label="wins")
                             result += f"    {status} Fight for '{target_faction}' at on-ground CZs in '{target_system}'" + "\n"
 
                             if target.get('settlements'):
                                 for settlement in target.get('settlements', []):
-                                    settlement_value: str = settlement.get('value', "0")
-                                    settlement_progress: str = settlement.get('progress', "0")
-
-                                    if settlement_value > 0 and settlement_progress >= settlement_value:
-                                        flag: str = "√ [done]"
-                                        complete: bool = True
-                                    else:
-                                        flag: str = "•"
-                                        complete: bool = False
-
-                                    status: str = flag if complete else f"{flag} [{settlement_progress} / {settlement_value} wins]"
-
+                                    status, value = self._get_status(settlement, label="wins")
                                     result += f"      {status} Fight at '{settlement.get('name', '<unknown>')}'" + "\n"
 
                         case MissionTargetType.SPACE_CZ:
-                            status: str = flag if complete else f"{flag} [{progress} / {value} wins]"
+                            status, value = self._get_status(target, label="wins")
                             result += f"    {status} Fight for '{target_faction}' at in-space CZs in '{target_system}'" + "\n"
 
                         case MissionTargetType.MURDER:
-                            status: str = flag if complete else f"{flag} [{progress} / {value} kills]"
+                            status, value = self._get_status(target, label="kills")
                             result += f"    {status} Murder ships of the '{target_faction}' in '{target_system}'" + "\n"
 
                         case MissionTargetType.MISSION_FAIL:
-                            status: str = flag if complete else f"{flag} [{progress} / {value} fails]"
+                            status, value = self._get_status(target, label="fails")
                             result += f"    {status} Fail missions against the '{target_faction}' in '{target_system}'" + "\n"
 
 
         return result
+
+
+    def _get_status(self, target: dict, numeric: bool = True, label: str|None = None) -> tuple[str, int]:
+        """Get a string showing the status of a particular mission or sub-mission
+
+        Args:
+            target (dict): A dict containing information about the mission or sub-mission
+            just_flag (bool): If True, only show a status flag, not the numeric progress. Defaults to False.
+            numeric (bool): If True, track as progress towards a numeric target. Defaults to True.
+            label (str | None, optional): A label suffix for the values. Defaults to None.
+
+        Returns:
+            tuple[str, int]: The description and the numeric target value
+        """
+        try:
+            value: int = int(target.get('value', 0))
+        except ValueError:
+            value: int = 0
+
+        try:
+            progress: int = int(target.get('progress', 0))
+        except ValueError:
+            progress: int = 0
+
+        if value == 0 and numeric:
+            flag: str = "∞"
+            complete: bool = False
+        elif (numeric and value > 0 and progress >= value) or \
+             (numeric and value < 0 and progress <= value) or \
+             (not numeric and progress > 0):
+            flag: str = "√ [done]"
+            complete: bool = True
+        else:
+            flag: str = "•"
+            complete: bool = False
+
+        if complete or not numeric:
+            # Don't show target value
+            return flag, value
+        elif value == 0:
+            # Infinite target value
+            return f"{flag} [{human_format(progress)} / ∞ {label}]", value
+        else:
+            # Integer target value
+            return f"{flag} [{human_format(progress)} / {human_format(value)} {label}]", value

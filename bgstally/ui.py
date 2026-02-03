c4a9b28d2918147a954a779fc254e380d741045d
Daniele Liprandi <daniele.liprandi@gmail.com>
2026-02-03 20:32:54 +0100

    use custom overlay

--- bgstally/ui.py
+++ bgstally/ui.py
@@ -633,7 +633,7 @@ class UI:
 
             # Objectives
             if self.bgstally.state.enable_overlay_objectives and self.bgstally.objectives_manager.get_objectives() != []:
-                objectives_text: str = self.bgstally.objectives_manager.get_human_readable_objectives(False)
+                objectives_text: str = self.bgstally.objectives_manager.get_overlay_objectives()
                 self.bgstally.overlay.display_message("objectives", objectives_text, fit_to_text=True, title=self.bgstally.objectives_manager.get_title())
 
             # Colonisation

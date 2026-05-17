d9ea46ac531c59e97c7509442ae33d107d624583
aussig <austin@opensceneryx.com>
2026-05-17 08:04:49 +0100

    Discord preview not refreshing when changing favourite faction mode Fixes #484

--- bgstally/windows/activity.py
+++ bgstally/windows/activity.py
@@ -483,8 +483,8 @@ class WindowActivity:
         """
         k: str = next(k for k, v in favourite_types.items() if v == value)
         self.bgstally.state.FavouriteActivityMode.set(k)
+        self.bgstally.state.refresh()
         self._update_discord_field(activity)
-        self.bgstally.state.refresh
 
 
     def _toggle_favourite_faction(self, faction_name:str, activity: Activity, label:ttk.Label, *args):

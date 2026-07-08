7c7029312553c6e899ea54b2ba7b371bd6f9e705
dwomble <derek-github@atuin.net>
2026-07-08 14:34:52 -0700

    Added UI state based hiding of overlays

--- load.py
+++ load.py
@@ -69,3 +69,10 @@ def capi_fleetcarrier(data: CAPIData):
     """
     if this.state.Status.get() != CheckStates.STATE_ON: return
     this.capi_fleetcarrier(data)
+
+def dashboard_entry(cmdr:str, is_beta:bool, entry:dict) -> None:
+    """
+    Handle dashboard entries
+    """
+    if this.state.Status.get() != CheckStates.STATE_ON: return
+    this.ui.dashboard_entry(cmdr, is_beta, entry)
